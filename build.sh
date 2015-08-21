00cb057091d105b643a16b62f89175c61de6bf8f
Daniel Nixon <dan.nixon@gmail.com>
2015-08-21 12:27:13 +1000

    Update build.sh

--- build.sh
+++ build.sh
@@ -12,3 +12,9 @@ export version="3.0" release="3.0.2"
 export slick_extensions_version="3.0.0"
 
 sphinx-build -b epub slick/slick/src/sphinx/ output
+
+if hash ebook-convert 2>/dev/null; then
+  ebook-convert ./output/Slick.epub ./output/Slick.mobi
+else
+  echo "Missing command 'ebook-convert', cannot convert to mobi. Try 'brew cask install calibre'."
+fi
