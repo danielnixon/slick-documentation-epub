a47eff4143aadaa70b3419e5f12e4fd4cc36f156
Daniel Nixon <dan.nixon@gmail.com>
2015-09-19 12:08:38 +1000

    Update to 3.0.3.

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ git submodule update
 
 command -v sphinx-build >/dev/null 2>&1 || { echo "Missing command 'sphinx-build'. Try 'pip install Sphinx'." >&2; exit 1; }
 
-export version="3.0" release="3.0.2"
+export version="3.0" release="3.0.3"
 export slick_extensions_version="3.0.0"
 
 sphinx-build -b epub slick/slick/src/sphinx/ output
